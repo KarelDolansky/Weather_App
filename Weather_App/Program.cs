3f0ba736f93ab0251a12361cadb44f8cf00e42dd
Karel_Dolansky <karel.dolansky@tul.cz>
2024-05-11 00:48:10 +0200

    database

--- Weather_App/Program.cs
+++ Weather_App/Program.cs
@@ -11,7 +11,7 @@ builder.Configuration.AddAzureKeyVault(keyVaultEndpoint, new DefaultAzureCredent
 
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+var connectionString = builder.Configuration.GetConnectionString("ConnectionStrings--try46") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
