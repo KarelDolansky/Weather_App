e940161bbe3dbcc3ad504965a20e8c6875da418d
Karel_Dolansky <karel.dolansky@tul.cz>
2024-05-20 21:49:30 +0200

    done

--- Weather_App/Services/WeatherServiceHandler.cs
+++ Weather_App/Services/WeatherServiceHandler.cs
@@ -2,7 +2,7 @@
 {
     public interface IWeatherServiceHandler
     {
-        public Task<string> CallApi(double latitude, double longitude, int days = 1);
+        public Task<string> CallApi(double latitude, double longitude, DateOnly date);
     }
 
     public class WeatherServiceHandler: IWeatherServiceHandler
@@ -13,9 +13,10 @@
             _httpClient = httpClient;
         }
 
-        public async Task<string> CallApi(double latitude, double longitude,int days=1)
+        public async Task<string> CallApi(double latitude, double longitude,DateOnly date)  
         {
-            string url = $"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&hourly=temperature_2m,precipitation,rain,showers,snowfall,snow_depth&timezone=Europe%2FBerlin&forecast_days={days}&daily=weather_code";
+            string url = $"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&hourly=temperature_2m,precipitation&daily=weather_code&timezone=Europe%2FBerlin&start_date={date.Year}-{date.Month:D2}-{date.Day:D2}&end_date={date.Year}-{date.Month:D2}-{date.Day:D2}";
+            //string url = $"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&hourly=temperature_2m,precipitation,rain,showers,snowfall,snow_depth&timezone=Europe%2FBerlin&forecast_days={days}&daily=weather_code";
             HttpResponseMessage response = await _httpClient.GetAsync(url);
 
             if (response.IsSuccessStatusCode)
